c286614a4caf3981c556910d56c1dbd84d7e1a2e
molly <mollynzembi@gmail.com>
2024-05-22 12:18:44 +0300

    first test

--- lib/looping.py
+++ lib/looping.py
@@ -3,6 +3,24 @@
 def happy_new_year():
     # code goes here!
     pass
+def happy_new_year():
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+
+# Example usage
+happy_new_year()
+
+
+# Example usage
+happy_new_year()
+
+
+# Example usage
+happy_new_year()
+
 
 def square_integers(int_list):
     # code goes here!
